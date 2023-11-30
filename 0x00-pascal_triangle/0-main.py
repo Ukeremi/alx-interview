05e1d23c369f936bbf3a23495c3a224f233146bc
Ukpono <ukpono9@gmail.com>
2023-11-30 09:38:32 +0000

    function that Import signUpUser from 4-user-promise.js and uploadPhoto from 5-photo-reject.js

--- 0x00-pascal_triangle/0-main.py
+++ 0x00-pascal_triangle/0-main.py
@@ -3,12 +3,14 @@
 0-main
 """
 pascal_triangle = __import__('0-pascal_triangle').pascal_triangle
-def print_trangle(triangle)
+
+def print_triangle(triangle):
     """
-    print the triangle
+    Print the triangle
     """
     for row in triangle:
         print("[{}]".format(",".join([str(x) for x in row])))
 
+
 if __name__ == "__main__":
-    print_trangle(pascal_triangle(5))
+    print_triangle(pascal_triangle(5))
--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 """
-pascal triangle function
+pascal triangle
 """
 
 def pascal_triangle(n):
-    if n == 0:
-        return []
-    elif n == 1:
-        return [[1]]
-    else:
-        new_row [1]
-        result = triangle(n-1)
-        last_row = result[-1]
-        for i in range(len(last_row)-1):
-            new_row.append(last_row[i] + last_row[i+1])
-            new_row += [1]
-            result.append(new_row)
-            return result
+    """
+    Return a list of integers
+    representing the Pascal Triangle of n 
+    returns empty list if n <= 0
+    """
+    k = []
+    if n <= 0:
+        return k
+    k = [[1]]
+    for i in range(1, n):
+        temp = [1]
+        for j in range(len(k[i-1]) - 1):
+            curr = k[i-1]
+            temp.append(k[i - 1][j] + k[i - 1][j +1])
+            temp.append(1)
+            k.append(temp)
+            return k
